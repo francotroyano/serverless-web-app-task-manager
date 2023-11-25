9968e5976bce59361c4f3c4fef7ba463698af75e
pacogit <elmor1@hotmail.com>
2023-11-25 19:45:54 +0100

    loadTaskFromStorage10

--- script.js
+++ script.js
@@ -35,7 +35,9 @@ function loadTasksFromStorage() {
     };
     // API call to retrieve tasks from DynamoDB
     fetch("https://jmayy9wgi3.execute-api.eu-west-1.amazonaws.com/dev", requestOptions)
-        .then(response => response.json())
+        .then(response => response.json()
+            alert(response.json());
+        )
         .then(result => {
             alert(result);
             let size = result.length;
