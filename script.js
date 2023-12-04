8bc025bd0e2415e3caa920164980f487925ef0c5
pacogit <elmor1@hotmail.com>
2023-12-04 17:14:22 +0100

    modified addTaskToList to consider task.ID5

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function loadTasksFromStorage() {
     fetch("https://jmayy9wgi3.execute-api.eu-west-1.amazonaws.com/dev", requestOptions)
         .then(response => {
             //let js = JSON.stringify(response);
-            let js = JSON.stringify({["idTask":"1701704021291","nameTask":"n","descriptionTask":"n","toggle":false}]);
+            let js = JSON.stringify([{"idTask":"1701704021291","nameTask":"n","descriptionTask":"n","toggle":false}]);
             alert (js);
             let size = JSON.parse(js).length;
             alert (size);
